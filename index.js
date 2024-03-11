dba9d22f93f6964ab87f3234605a89a4ee22ad4d
Altieri Palorca <altierirpalorca@gmail.com>
2024-03-11 14:00:06 -0300

    1.2 captura de fotos funcionando

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then(function (data) {
         const pokemons = data.results;
 
-
         const enviar = document.getElementById("enviar")
         const divName = document.getElementById("name")
 
@@ -15,22 +14,37 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
             const pokemonIndex = parseInt(input) - 1; // Ajusta o índice do array para começar em 0
             if (pokemonIndex >= 0 && pokemonIndex < pokemons.length) {
                 const pokemonNome = pokemons[pokemonIndex].name
-                divName.innerText = pokemonNome;
+                divName.innerText = pokemonNome.toUpperCase().slice();
+
 
                 fetch(`https://pokeapi.co/api/v2/pokemon/${input}/`)//verificar se os dados que quero estão aqui mesmo, ou busco outros dados
                     .then(function (response) {
                         return response.json();
                     })
                     .then(function (pokemonData) {
+                        /* ------------------------- RETORNO TIPO ------------------------------ */
                         console.log(pokemonData);//subistituir pelo retorno em tela da nova div
-                        typeResult.innerText = pokemonData.types[0].type.name
+                        typeResult.innerText = pokemonData.types[0].type.name.charAt(0).toUpperCase() + pokemonData.types[0].type.name.slice(1)
                         //retornar a imagem do pokémon
+                        /* ---------------------------- RETORNO IMAGEM--------------------------------- */
+                        const imageUrl = pokemonData.sprites.front_default;
+                        const divImagem = document.getElementById('imagem');
+
+                        divImagem.innerHTML = ''//limpa a div no html ante da proxima execução, quando executo o código de novo clicando, ele começa a leitura, lê tudo em cima, quando chega aqui limpa a div da imagem e retorna a nova
+
+                        const img = document.createElement('img');
+                        img.src = imageUrl//coloca o resultado da requisição como src na div imagem
+                        divImagem.appendChild(img);//Esta linha pega o elemento de imagem recém-criado (img) e o anexa como um elemento filho ao elemento <div> com o ID "imagem". Isso essencialmente insere a imagem dentro da <div> em sua página da web.
                     })
+
+
+
 
             } else {
                 divName.innerText = "Invalid Number";
             }
             console.log(data)
+
         });
 
 
@@ -44,7 +58,8 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
 /* ----------------------------- Próximos Passos ---------------------------- */
 
 // OK - Colocar o retorno do número digitado para aparecer na div embaixo da foto
-// - Descobrir como fazer aparecer a foto quando for retornar o número
+// OK - Foto quando for retornar o número
 // - Colocar select com os pokemons
+// - Linkar select e número para um retornar o outro e ai retornar os dados na section da direita
 // - Fazer outra solicitação para captura dos poderes, tipo ou outra informação?
 // - Colocar contra o que é fraco ou forte
