98be253866a4f0a9d4aa56a185d2f5e821fb9602
Altieri Palorca <altierirpalorca@gmail.com>
2024-03-13 11:37:42 -0300

    1.3 select retornando numero

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
 
         enviar.addEventListener("click", function () {
 
+            
+
             const input = document.getElementById("inputNumber").value
             const pokemonIndex = parseInt(input) - 1; // Ajusta o índice do array para começar em 0
             if (pokemonIndex >= 0 && pokemonIndex < pokemons.length) {
@@ -23,13 +25,13 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
                     })
                     .then(function (pokemonData) {
 
-/* ------------------------- RETORNO TIPO ------------------------------ */
+                        /* ------------------------- RETORNO TIPO ------------------------------ */
 
                         console.log(pokemonData);//subistituir pelo retorno em tela da nova div
                         typeResult.innerText = pokemonData.types[0].type.name.charAt(0).toUpperCase() + pokemonData.types[0].type.name.slice(1)
                         //retornar a imagem do pokémon
 
-/* ---------------------------- RETORNO IMAGEM--------------------------------- */
+                        /* ---------------------------- RETORNO IMAGEM--------------------------------- */
 
                         const imageUrl = pokemonData.sprites.front_default;
                         const divImagem = document.getElementById('imagem');
@@ -57,7 +59,7 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         console.log("Houve um erro: " + error);
     });
 
-/* ------------------------------ Teste Select ------------------------------ */
+/* ------------------------------ Select ------------------------------ */
 
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .then(function (response) {
@@ -65,26 +67,64 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     })
     .then(function (data) {
         const pokemons = data.results;
-        const select = document.getElementById("testeSelect")
-        
+        const select = document.getElementById("select")
+
 
         pokemons.forEach((pokemon) => {
             const option = document.createElement("option");
+            //Entender esse 6 abaixo e o split
             option.value = pokemon.url.split("/")[6]; // Extrai o ID do pokemon da URL
             option.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
             select.appendChild(option);
-            
+
+        })
+        
+        select.addEventListener("change", function () {
+            const pokemonId = select.value;
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json(); // Analise apenas se a resposta for bem-sucedida
+                    } else {
+                        console.error(`Erro: Pokémon com ID ${pokemonId} não encontrado.`);
+                        // Trate o erro (por exemplo, exiba uma mensagem ao usuário)
+                        return null; // Evita processamento posterior (opcional)
+                    }
+                })
+
+                .then(function (pokemonData) {
+                    const imageUrl = pokemonData.sprites.front_default;
+                    const divImagem = document.getElementById('imagem');
+
+                    divImagem.innerHTML = "";
+
+                    const img = document.createElement('img');
+                    img.src = imageUrl//coloca o resultado da requisição como src na div imagem
+                    divImagem.appendChild(img);//Esta linha pega o elemento de imagem recém-criado (img) e o anexa como um elemento filho ao elemento <div> com o ID "imagem". Isso essencialmente insere a imagem dentro da <div> em sua página da web.
+                    typeResult.innerText = pokemonData.types[0].type.name.charAt(0).toUpperCase() + pokemonData.types[0].type.name.slice(1)
+
+                    inputNumber.value = pokemonId
+                    
+                    
+
+                })
+                .catch(function (error) {
+                    console.error("Erro:", error);
+                })
+                
         })
     })
     .catch(function (error) {
         console.error("Erro:", error); // Use console.error para erros
     });
+
 /* ----------------------------- Próximos Passos ---------------------------- */
 
 // OK - Colocar o retorno do número digitado para aparecer na div embaixo da foto
 // OK - Foto quando for retornar o número
 // OK - Colocar select com os pokemons
-// - Mover select para dentro do outro escopo para usar a mesma solicitação de api
 // - Linkar select e número para um retornar o outro e ai retornar os dados na section da direita
+//     ->select já estpa retornado numero, mas numero ainda não retorna o select
+
 // - Fazer outra solicitação para captura dos poderes, tipo ou outra informação?
 // - Colocar contra o que é fraco ou forte
